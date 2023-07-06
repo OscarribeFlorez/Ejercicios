a4d128364b02ac66e82d6f2904f512304a8a7682
oscar Uribe <oscaruribe25f@gmail.com>
2023-07-05 19:17:48 -0500

    se trabajo con varibles y conetivos logicos

---
+++ Varibles.js
@@ -0,0 +1,20 @@
+// clase de hoy se trebajara el manjeo de varibles en javascript 
+// tipos de variable Var-let-const alcnses de cada una 
+
+var estudiante ="hola estudiantes ";// esta varible se declara en la parte superio del codigo 
+console.log(estudiante);
+
+                                    //se utiliza como una varible global la cual puedo utilizar en cualquier funcion 
+let estudiante1="soy estudiante "//variables mutables 
+const estudiante2="soy de giron ";//varibles inmutables no se puede cambiar 
+
+console.log(estudiante1);
+console.log(estudiante2);
+// TYPESCRIPT ES PARA CONTROLAR ERRORES EN LA DECLARACION DE VARIBLES 
+// tipos de varibles segun su propocito 
+let edad= 37;
+let nombre="oscar";
+let estado= true;
+let pesos=98.5;
+let personajes={};
+let arraPersonajes=[];
