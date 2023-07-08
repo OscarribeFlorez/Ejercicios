19bd5e2a3a65361357fa4ef1a1c14abab7231128
oscar Uribe <oscaruribe25f@gmail.com>
2023-07-07 19:58:48 -0500

    calculadora

---
+++ Calculadora/calculadora.js
@@ -0,0 +1,26 @@
+// resulatados como mostrar el resultado 
+
+// funcion para resultados 
+let chainNu="";
+const resultadoCalculadora=()=>{
+     document.getElementById("resultado").innerHTML=chainNu;
+};
+
+//funcion selecionar numero 
+const concatNumero = (numberChar)=>{
+     chainNu+=numberChar;
+     resultadoCalculadora();
+};
+
+// funcion suma 
+
+const sumaNumero =()=>{
+     arraNumero.push(parseFloat(chainNu));
+     chainNu="";
+     let sumaNumero=0;
+     arraNumero.array.forEach(element => {
+          sumaNumero+=
+          
+     });
+
+};
